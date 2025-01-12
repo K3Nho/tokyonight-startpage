9a1067e0aed1492e6f30e57db1700afaeb813133
K3Nho <156653376+K3Nho@users.noreply.github.com>
2025-01-12 17:24:53 +0800

    Update statusbar.component.js

--- src/components/statusbar/statusbar.component.js
+++ src/components/statusbar/statusbar.component.js
@@ -2,24 +2,16 @@ class Statusbar extends Component {
   externalRefs = {};
 
   refs = {
-    categories: ".categories ul",
-    tabs: "#tabs ul li",
-    indicator: ".indicator",
     fastlink: ".fastlink",
   };
 
-  currentTabIndex = 0;
-
   constructor() {
     super();
-
     this.setDependencies();
   }
 
   setDependencies() {
-    this.externalRefs = {
-      categories: this.parentNode.querySelectorAll(this.refs.categories),
-    };
+    this.externalRefs = {}; // 此处可以清空，因为其他引用已移除
   }
 
   imports() {
@@ -30,12 +22,6 @@ class Statusbar extends Component {
     return `
       *:not(:defined) { display: none; }
 
-      #tabs,
-      #tabs .widgets,
-      #tabs ul li:last-child {
-          position: absolute;
-      }
-
       #tabs {
           width: 100%;
           height: 100%;
@@ -45,7 +31,7 @@ class Statusbar extends Component {
           right: 0;
           margin: auto;
           height: 32px;
-          : #fff;
+          color: #fff;
           font-size: 12px;
       }
 
@@ -72,29 +58,6 @@ class Statusbar extends Component {
           background: rgba(255, 255, 255, .05);
       }
 
-      #tabs > cols {
-          position: relative;
-          grid-template-columns: [chat-tab] 35px [tabs] auto [widgets] auto;
-      }
-
-      #tabs .time span {
-          font-weight: 400;
-      }
-
-      #tabs i {
-          font-size: 14pt !important;
-      }
-
-      .widget:not(:first-child)::before {
-          content: '';
-          position: absolute;
-          display: block;
-          left: 0;
-          height: calc(100% - 15px);
-          width: 1px;
-          background: rgb(255 255 255 / 10%);
-      }
-
       .fastlink {
           border: 0;
           background: #181825;
@@ -118,9 +81,8 @@ class Statusbar extends Component {
         <div id="tabs">
             <cols>
                 <button class="+ fastlink">
-                  <img class="fastlink-icon" src="src/img/logo.png"/>
+                  <img class="fastlink-icon" src="src/img/anime.svg"/>
                 </button>
-                <ul class="- indicator"></ul>
                 <div class="+ widgets col-end">
                     <current-time class="+ widget"></current-time>
                     <weather-forecast class="+ widget weather"></weather-forecast>
@@ -130,94 +92,18 @@ class Statusbar extends Component {
   }
 
   setEvents() {
-    this.refs.tabs.forEach((tab) => (tab.onclick = ({ target }) => this.handleTabChange(target)));
-
-    document.onkeydown = (e) => this.handleKeyPress(e);
-    document.onwheel = (e) => this.handleWheelScroll(e);
+    // 保留 fastlink 的点击事件
     this.refs.fastlink.onclick = () => {
       console.log(CONFIG.fastlink);
       if (CONFIG.config.fastlink) {
         window.location.href = CONFIG.config.fastlink;
       }
     };
-
-    if (CONFIG.openLastVisitedTab) {
-      window.onbeforeunload = () => this.saveCurrentTab();
-    }
-  }
-
-  saveCurrentTab() {
-    localStorage.lastVisitedTab = this.currentTabIndex;
-  }
-
-  openLastVisitedTab() {
-    if (!CONFIG.openLastVisitedTab) return;
-    this.activateByKey(localStorage.lastVisitedTab);
-  }
-
-  handleTabChange(tab) {
-    this.activateByKey(Number(tab.getAttribute("tab-index")));
-  }
-
-  handleWheelScroll(event) {
-    if (!event) return;
-
-    let { target, wheelDelta } = event;
-
-    if (target.shadow && target.shadow.activeElement) return;
-
-    let activeTab = -1;
-    this.refs.tabs.forEach((tab, index) => {
-      if (tab.getAttribute("active") === "") {
-        activeTab = index;
-      }
-    });
-
-    if (wheelDelta > 0) {
-      this.activateByKey((activeTab + 1) % (this.refs.tabs.length - 1));
-    } else {
-      this.activateByKey(activeTab - 1 < 0 ? this.refs.tabs.length - 2 : activeTab - 1);
-    }
-  }
-
-  createTabs() {
-    const categoriesCount = this.externalRefs.categories.length;
-
-    for (let i = 0; i <= categoriesCount; i++) {
-      this.refs.indicator.innerHTML += `<li tab-index=${i} ${i == 0 ? "active" : ""}></li>`;
-    }
-  }
-  
-  handleKeyPress(event) {
-    if (!event) return;
-
-    let { target, key } = event;
-
-    if (target.shadow && target.shadow.activeElement) return;
-
-    if (Number.isInteger(parseInt(key)) && key <= this.externalRefs.categories.length) {
-      this.activateByKey(key - 1);
-    }
-  }
-
-  activateByKey(key) {
-    if (key < 0) return;
-    this.currentTabIndex = key;
-
-    this.activate(this.refs.tabs, this.refs.tabs[key]);
-    this.activate(this.externalRefs.categories, this.externalRefs.categories[key]);
-  }
-
-  activate(target, item) {
-    target.forEach((i) => i.removeAttribute("active"));
-    item.setAttribute("active", "");
   }
 
   connectedCallback() {
     this.render().then(() => {
-      this.createTabs();
-      this.setEvents();
-      this.openLastVisitedTab();
+      this.setEvents(); // 仅绑定 fastlink 的事件
     });
   }
 }
