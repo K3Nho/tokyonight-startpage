d8605e94b2f1ab01f40bfc62d3bf39f1b17b7da9
K3Nho <156653376+K3Nho@users.noreply.github.com>
2025-01-20 16:10:18 +0800

    Update statusbar.component.js

--- src/components/statusbar/statusbar.component.js
+++ src/components/statusbar/statusbar.component.js
@@ -1,4 +1,8 @@
 class Statusbar extends Component {
+  refs = {
+    fastlink: ".fastlink",
+  };
+
   constructor() {
     super();
   }
@@ -11,14 +15,17 @@ class Statusbar extends Component {
     return `
       *:not(:defined) { display: none; }
 
-      #tabs {
-          width: 100%;
-          height: 100%;
+      #tabs,
+      #tabs .widgets {
           position: absolute;
       }
 
+      #tabs {
+          width: 100%;
+          height: 100%;
+      }
+
       .widgets {
-          position: absolute;
           right: 0;
           margin: auto;
           height: 32px;
@@ -26,27 +33,50 @@ class Statusbar extends Component {
           font-size: 12px;
       }
 
-      .widget {
-          height: 100%;
-          padding: 0 1em;
-          position: relative;
-          cursor: pointer;
+      .widgets:hover .edit {
+          margin: 0;
       }
 
-      .widget:first-child { padding-left: 2em; }
-      .widget:last-child { padding-right: 2em; }
+      .widget {
+          position: relative;
+          height: 100%;
+          padding: 0 1em;
+      }
+
+      .widget:first-child {
+          padding-left: 2em;
+      }
+
+      .widget:last-child {
+          padding-right: 2em;
+      }
 
       .widget:hover {
+          cursor: pointer;
           background: rgba(255, 255, 255, .05);
+      }
+
+      #tabs > cols {
+          position: relative;
+          grid-template-columns: [chat-tab] 35px [tabs] auto [widgets] auto;
+      }
+
+      #tabs .time span {
+          font-weight: 400;
+      }
+
+      #tabs i {
+          font-size: 14pt !important;
       }
 
       .widget:not(:first-child)::before {
           content: '';
           position: absolute;
+          display: block;
           left: 0;
           height: calc(100% - 15px);
           width: 1px;
-          background: rgba(255, 255, 255, 0.1);
+          background: rgb(255 255 255 / 10%);
       }
 
       .fastlink {
@@ -55,10 +85,6 @@ class Statusbar extends Component {
           color: #a6e3a1;
           cursor: pointer;
           border-radius: 5px 15px 15px 5px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          padding: 0.5em;
       }
 
       .fastlink:hover {
@@ -66,33 +92,38 @@ class Statusbar extends Component {
       }
 
       .fastlink-icon {
-          width: 70%;
+        width: 70%;
       }
     `;
   }
 
   template() {
     return `
-      <div id="tabs">
-        <button class="fastlink">
-          <img class="fastlink-icon" src="src/img/logo.png" />
-        </button>
-        <div class="widgets">
-          <current-time class="widget"></current-time>
-          <weather-forecast class="widget"></weather-forecast>
-        </div>
-      </div>`;
+        <div id="tabs">
+            <cols>
+                <button class="+ fastlink">
+                  <img class="fastlink-icon" src="src/img/logo.png"/>
+                </button>
+                <div class="+ widgets col-end">
+                    <current-time class="+ widget"></current-time>
+                    <weather-forecast class="+ widget weather"></weather-forecast>
+                </div>
+            </cols>
+        </div>`;
   }
 
   setEvents() {
-    document.querySelector(".fastlink").onclick = () => {
-      if (CONFIG?.config?.fastlink) {
+    this.refs.fastlink.onclick = () => {
+      console.log(CONFIG.fastlink);
+      if (CONFIG.config.fastlink) {
         window.location.href = CONFIG.config.fastlink;
       }
     };
   }
 
   connectedCallback() {
-    this.render().then(() => this.setEvents());
+    this.render().then(() => {
+      this.setEvents();
+    });
   }
 }
