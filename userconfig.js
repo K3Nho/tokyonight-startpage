bedb8810c6a66bf2495e0a4f699e40a375971c32
K3Nho <156653376+K3Nho@users.noreply.github.com>
2026-06-25 16:20:09 +0800

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -109,7 +109,7 @@ const default_config = {
           {
             name: "bitget",
             url: "https://www.bitget.com/zh-CN/spot/REWYUSDT",
-            icon: "brand-bebo",
+            icon: "brand-beats",
             icon_color: "#7dcfff",
           },
           {
